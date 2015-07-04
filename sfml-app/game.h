db4953e838fcebb7837545bfb8eed7b596118bf8
AppleKid7 <kvltd00d@gmail.com>
2015-07-04 02:00:16 -0400

    restarted game state manager

---
+++ sfml-app/game.h
@@ -0,0 +1,25 @@
+#include <stack>
+#include <SFML/Graphics.hpp>
+
+class GameState;
+
+class Game
+{
+public:
+
+	std::stack<GameState*> states;
+
+	sf::RenderWindow window;
+
+	void pushState(GameState* state);
+	void popState();
+	void changeState(GameState* state);
+	GameState* peekState();
+
+	void gameLoop();
+
+	Game();
+	~Game();
+};
+
+#endif /* GAME_HPP */
